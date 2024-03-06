bad057e3b2db60a1e16437575c99fef9e0ff4f35
michele annunziata <michele8028@live.it>
2024-03-06 18:11:03 +0100

    user story 1.1

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,44 @@
+<x-layout>
+ 
+    <div class="container-fluid p-5 bg-info text-center text-white">
+        <div class="row justify-content-center">
+            <h1 class="display-1">
+                Accedi
+            </h1>
+        </div>
+    </div>
+
+    <div class="container my-5">
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-8">
+                @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+                @endif
+
+                <form class="card p-5 shadow" action="{{ route('login') }}" method="POST">
+                    @csrf
+                   
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email</label>
+                        <input type="email" name="email" class="form-control" id="email" value="{{ old('email') }}">
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Password:</label>
+                        <input type="password" name="password" class="form-control" id="password" >
+                    </div>
+                 
+                    <div class="mb-3">
+                        <button class="btn bg-info text-white">Accedi</button>
+                        <p class="small mt-2">Non sei registrato? <a href="{{ route('register') }}">Clicca qui</a> </p>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</x-layout>
---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,44 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="{{ route('homepage') }}">AulabPost</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+
+                @auth
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        Benvenuto {{ Auth::user()->name }}
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li class="nav-item">
+                            <form action="{{ route('logout') }}" id="logout-form" method="POST">
+                                @csrf
+                                <button type="submit" class="btn nav-link">Logout</button>
+                            </form>
+                        </li>
+                    </ul>
+                </li>
+                @endauth
+
+                @guest
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        Benvenuto Ospite
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="{{ route('register') }}">Registrati</a></li>
+                        <li><a class="dropdown-item" href="{{ route('login') }}">Accedi</a></li>
+                    </ul>
+                </li>
+                @endguest
+
+            </ul>
+        </div>
+    </div>
+</nav>
