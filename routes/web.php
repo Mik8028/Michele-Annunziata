1744ac586dfd44e60f78ad44806019106630b615
Marco Accetta <marco.accetta.ct@gmail.com>
2024-03-07 12:21:46 +0100

    User story 1 completata

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PublicController;
-
+use App\Http\Controllers\ArticleController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,6 +16,6 @@ use App\Http\Controllers\PublicController;
 
 Route::get('/', [PublicController::class , 'homepage']) ->name('homepage');
 
+Route::get('/articles/create',[ArticleController::class,'create'])->name('article.create');
 
-
-
+Route::post('/article/store',[ArticleController::class, 'store'])->name('article.store');
