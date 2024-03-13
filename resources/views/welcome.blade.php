8382cf580e8745921d5a28f53dabf35ce9c133aa
Francesca Nichele <francesca.nichel@gmail.com>
2024-03-13 20:04:13 +0100

    homepage seconda parte

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,4 +65,64 @@
                     </div>
                 </div>
 
+
+        
+    <!-- inizio form accedi -->
+
+
+
+    <div class="container card p-5 shadow">
+        <div class="row">
+            <div class="col-12">
+                <div class="container my-5">
+        <div class="row justify-content-center">
+            <div class="row">
+            
+            <div class="col-12 col-md-6">
+                <h1>ciaoo</h1>
+            </div>
+            <div class="col-12 col-md-6">
+                @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+                @endif
+
+                <form class="card p-5 shadow" action="{{ route('login') }}" method="POST">
+                    @csrf
+
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email</label>
+                        <input type="email" name="email" class="form-control" id="email" value="{{ old('email') }}">
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Password:</label>
+                        <input type="password" name="password" class="form-control" id="password" >
+                    </div>
+
+                    <div class="mb-3">
+                        <button class="btn bg-info text-white">Accedi</button>
+                        <p class="small mt-2">Non sei registrato? <a href="{{ route('register') }}">Clicca qui</a> </p>
+                    </div>
+                </form>
+            </div>
+
+            
+
+        </div>
+    </div>
+</div>
+            </div>
+        </div>
+    </div>
+
+    
+
+
+    <!-- fine form accedi -->
+
 </x-layout>
