b41002924a6d49158c2e639f5ef05a2071c48612
michele annunziata <michele8028@live.it>
2024-03-19 22:41:03 +0100

    email completa

--- resources/views/mail/career-request-mail.blade.php
+++ resources/views/mail/career-request-mail.blade.php
@@ -1,16 +1,290 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+<head> -->
+<!-- <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Document</title>
 </head>
 <body>
-    <h1>Abbiamo ricevuto</h1>
-    <h4>una richiesta per il ruolo di {{ $info['role'] }}</h4>
-    <p>Ricevuta da {{ $info['email'] }}</p>
+<h1>Abbiamo ricevuto</h1>
+<h4>una richiesta per il ruolo di {{ $info['role'] }}</h4>
+<p>Ricevuta da {{ $info['email'] }}</p>
 
-    <h4>Messaggio</h4>
-    <p>{{ $info['message'] }}</p>
+<h4>Messaggio</h4>
+<p>{{ $info['message'] }}</p>
 </body>
+</html> -->
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html dir="ltr" xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office">
+
+<head>
+<meta charset="UTF-8">
+<meta content="width=device-width, initial-scale=1" name="viewport">
+<meta name="x-apple-disable-message-reformatting">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta content="telephone=no" name="format-detection">
+<title></title>
+
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<!--[if (mso 16)]>
+<style type="text/css">
+a {text-decoration: none;}
+</style>
+<![endif]-->
+<!--[if gte mso 9]><style>sup { font-size: 100% !important; }</style><![endif]-->
+<!--[if gte mso 9]>
+<xml>
+<o:OfficeDocumentSettings>
+<o:AllowPNG></o:AllowPNG>
+<o:PixelsPerInch>96</o:PixelsPerInch>
+</o:OfficeDocumentSettings>
+</xml>
+<![endif]-->
+</head>
+
+<body>
+<div dir="ltr" class="es-wrapper-color">
+<!--[if gte mso 9]>
+<v:background xmlns:v="urn:schemas-microsoft-com:vml" fill="t">
+<v:fill type="tile" color="#fafafa"></v:fill>
+</v:background>
+<![endif]-->
+<table class="es-wrapper" width="100%" cellspacing="0" cellpadding="0">
+<tbody>
+<tr>
+<td class="esd-email-paddings" valign="top">
+<table cellpadding="0" cellspacing="0" class="es-content esd-header-popover" align="center">
+<tbody>
+<tr>
+<td class="esd-stripe esd-synchronizable-module" align="center">
+<table class="es-content-body" align="center" cellpadding="0" cellspacing="0" width="600" style="background-color: transparent;" bgcolor="rgba(0, 0, 0, 0)">
+<tbody>
+<tr>
+<td class="esd-structure es-p20" align="left">
+<table cellpadding="0" cellspacing="0" width="100%">
+<tbody>
+<tr>
+<td width="560" class="esd-container-frame" align="center" valign="top">
+<table cellpadding="0" cellspacing="0" width="100%">
+<tbody>
+<tr>
+<td align="center" class="esd-block-text es-infoblock">
+
+</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+<table cellpadding="0" cellspacing="0" class="es-header" align="center">
+<tbody>
+<tr>
+<td class="esd-stripe esd-synchronizable-module" align="center">
+<table bgcolor="#ffffff" class="es-header-body" align="center" cellpadding="0" cellspacing="0" width="600">
+<tbody>
+<tr>
+<td class="esd-structure es-p10t es-p10b es-p20r es-p20l" align="left">
+<table cellpadding="0" cellspacing="0" width="100%">
+<tbody>
+<tr>
+<td width="560" class="es-m-p0r esd-container-frame" valign="top" align="center">
+<table cellpadding="0" cellspacing="0" width="100%">
+
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+<table cellpadding="0" cellspacing="0" class="es-content" align="center">
+<tbody>
+<tr>
+<td class="esd-stripe" align="center">
+<table bgcolor="#ffffff" class="es-content-body" align="center" cellpadding="0" cellspacing="0" width="600">
+<tbody>
+<tr>
+<td class="esd-structure es-p20t es-p20r es-p20l" align="left">
+<!--[if mso]><table width="560" cellpadding="0" cellspacing="0"><tr><td width="113" valign="top"><![endif]-->
+
+
+<!--[if mso]></td><td width="20"></td><td width="427" valign="top"><![endif]-->
+<table cellpadding="0" cellspacing="0" align="center">
+<tbody>
+<tr>
+<td width="427" align="left" class="esd-container-frame">
+<table cellpadding="0" cellspacing="0" width="100%">
+<tbody>
+<tr>
+
+<td align="right">
+<a target="_blank"><img src="{{ asset('storage/images/logo10.png') }}" alt style="display: block;" width="75"></a>
+</td>
+
+
+<td align="left" class="esd-block-text es-p20t es-p10b es-m-txt-c p-3">
+
+
+
+<h1 style="font-size: 46px; line-height: 100%;">AulabPost</h1>
+
+</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+<!--[if mso]></td></tr></table><![endif]-->
+</td>
+</tr>
+<tr>
+<td class="esd-structure es-p20t es-p20r es-p20l" align="left">
+<table cellpadding="0" cellspacing="0" width="100%">
+<tbody>
+<tr>
+<td width="560" class="esd-container-frame" align="center" valign="top">
+<table cellpadding="0" cellspacing="0" width="100%">
+<tbody>
+
+<h2 class=" align-content-center pt-3 ">Richiesta di ruolo</h2>
+
+
+<tr>
+<td align="center" class="esd-block-image es-p10t p-3" style="font-size: 0px;"><a target="_blank"><img class="adapt-img" src="{{ asset('storage/images/00004.jpeg') }}" alt style="display: block;" width="560"></a></td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+<tr>
+<td width="560" class="esd-container-frame" align="center" valign="top">
+<table cellpadding="0" cellspacing="0" width="100%">
+<tbody>
+
+<h3 class=" align-content-center ">ricevuta da: {{ $info['email'] }}</h3>
+
+
+
+<tr>
+<td align="left" class="esd-block-text es-p5t es-p5b p-2">
+
+<p>{{ $info['message'] }}</p>
+</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+<tr>
+<td class="esd-structure es-p10t es-p20b es-p20r es-p20l" align="left">
+<table cellpadding="0" cellspacing="0" width="100%">
+<tbody>
+<tr>
+<td width="560" class="esd-container-frame" align="left">
+<table cellpadding="0" cellspacing="0" width="100%">
+<tbody>
+
+
+
+<div align="center">
+<button class="btn btn-warning"><a style="text-decoration: none; color:black " href="http://127.0.0.1:8000/admin/dashboard">Link alla dashboard</a></button>
+
+
+
+
+
+</div>
+
+
+
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+<table cellpadding="0" cellspacing="0" class="es-footer" align="center">
+<tbody>
+<tr>
+<td class="esd-stripe esd-synchronizable-module" align="center">
+<table class="es-footer-body" align="center" cellpadding="0" cellspacing="0" width="640" style="background-color: transparent;">
+<tbody>
+<tr>
+<td class="esd-structure es-p20t es-p20b es-p20r es-p20l" align="left">
+<table cellpadding="0" cellspacing="0" width="100%">
+<tbody>
+<tr>
+<td width="600" class="esd-container-frame" align="left">
+<table cellpadding="0" cellspacing="0" width="100%">
+<tbody>
+
+<tr>
+
+<td align="center" class="esd-block-text es-p35b p-2">
+
+<p>© 2023-2024 The Coders</p>
+</td>
+</tr>
+
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+
+</td>
+</tr>
+</tbody>
+</table>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
 </html>
