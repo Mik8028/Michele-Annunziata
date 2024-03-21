aa5007b42eb8a440994500ba23f3071b5ee17f87
Marco Accetta <marco.accetta.ct@gmail.com>
2024-03-21 20:03:18 +0100

    carosello sistemato

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,7 +68,7 @@
   <div class="container my-5">
     <div class="row justify-content-center">
       @foreach($articles as $article)
-      <div class="col-12 col-md-3 pb-5">
+      <div class="col-12 col-md-4 pb-5 col-lg-3">
 
         <x-card 
           :tags="$article->tags"
