275415e9d0cd10f4473351bc4fe798a12cbd03e6
Francesca Nichele <francesca.nichel@gmail.com>
2024-03-22 18:59:50 +0100

    sistemazione apostrofo

--- resources/views/article/by-editor.blade.php
+++ resources/views/article/by-editor.blade.php
@@ -19,15 +19,15 @@
                 
                 <x-card 
                     :tags="$article->tags"
-                    title="{{ $article->title }}" 
-                    subtitle="{{ $article->subtitle }}" 
-                    image="{{ $article->image }}"
-                    category="{{ $article->category?->name }}" 
-                    data="{{ $article->created_at->format('d/m/Y') }}"
-                    user="{{ $article->user->name }}" 
-                    url="{{ route('article.show' , compact('article')) }}"
-                    urlCategory=" {{ $article->category ? route('article.byCategory', ['category' => $article->category->id]) : null }}"
-                    urlEditor=" {{ route('article.byEditor', ['user' => $article->user->id]) }}"
+                    :title="$article->title"
+                    :subtitle=" $article->subtitle" 
+                    :image="$article->image " 
+                    :category="$article->category?->name" 
+                    :data=" $article->created_at->format('d/m/Y') " 
+                    :user=" $article->user->name " 
+                    :url="route('article.show' , compact('article'))" 
+                    :urlCategory=" $article->category ? route('article.byCategory', ['category' => $article->category->id]) : null " 
+                    :urlEditor="route('article.byEditor', ['user' => $article->user->id])"
                 />
             </div>
             @endforeach
